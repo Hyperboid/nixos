c43d09250a5d3e58408491d5e5aae2ab2047337b
Hyperboid <23138363+Hyperboid@users.noreply.github.com>
2025-04-11 11:23:48 -0400

    Enable sshd

--- common.nix
+++ common.nix
@@ -200,7 +200,7 @@ in {
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
   services.kanata = {
     enable = true;
     keyboards = {
