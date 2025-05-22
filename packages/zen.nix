a8609a51e306b83408d765e779fcde3c0596e51b
Hyperboid <23138363+Hyperboid@users.noreply.github.com>
2025-05-21 23:47:04 -0400

    Update zen again

--- packages/zen.nix
+++ packages/zen.nix
@@ -9,7 +9,7 @@
 
   src = fetchurl {
     url = "https://github.com/zen-browser/desktop/releases/latest/download/zen-x86_64.AppImage";
-    sha256 = "sha256-aRFKjSw3Wove0k091ndEBjbduxEWOMymfmZpWNZqNVk=";
+    sha256 = "sha256-cP3FHbWsAKTRcX0ysK68D/rSD16mlE/yBDeNq6y8gpE=";
   };
 
   appimageContents = appimageTools.extract {
